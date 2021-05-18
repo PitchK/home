97241dde016bc0883d891c6fc35c2cd3819e3599
M K <mchlcape@gmail.com>
2021-05-17 22:20:49 -1000

    sort of fixed sticky header

--- js/app.js
+++ js/app.js
@@ -1,37 +1,46 @@
-
-
-var close = document.getElementById('X');
-close.addEventListener("click", removeHeader);
-
-var deleted = document.getElementById('header');
+var head = document.getElementById('header');
 var newTitle = document.getElementById('title');
+var button = document.getElementById('x');
 
-/*function dropHeader(){
-    deleted.style.transform = "translate(0px, 0%)";
-    deleted.style.transition = "1.5s";
-    newTitle.style.transform = "translate(0px, 0%)";
-    newTitle.style.transition = "1.5s";
+function dropHeader(){
+
+    head.style.position = 'absolute';
+    
+    head.style.display = 'block';
+    head.style.display = 'flex';
+    head.style.width = '100%';
+
+    head.style.top = '-2%';
+
+    head.style.transition = '1s';
+
+
 }
 
 function removeHeader(){
+    head.style.position = 'absolute';
+    head.style.top = '-100%';
+    head.style.transition = '0.5s';
+}
 
-    deleted.style.transform = "translate(0px, -120%)";
-    deleted.style.transition = "1s";
-    /*newTitle.style.transform = "translate(0px, -120%)";
-    newTitle.style.transition = "1s";
-}*/
 
-/*function updateScroll(){
-    var scr = this.scrollY
-    console.log(scr);
-    newTitle.innerHTML = scr;
-    if(scr > 8.666){
-        document.getElementById("header").style.top = scr;
+
+
+
+function updateScrollBar(){
+    ;
+    if (head.style.top !== '-100%'){
+        var test = 0;
+        var scr = this.scrollY
+        console.log(scr);
+        header.style.top = (scr-10) + "px";
+        header.style.transition = '0s';
     }
-}*/
+
+}
 
 
+window.addEventListener('scroll', updateScrollBar);
+button.addEventListener('click', removeHeader);
+setTimeout(dropHeader, 2000);
 
-/*window.addEventListener("scroll", updateScroll);*/
-
-/*setTimeout(dropHeader, 3000);
